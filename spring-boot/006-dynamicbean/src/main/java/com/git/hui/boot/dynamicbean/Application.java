833f00235b3b4952681e0961d7307bafcdffd627
yihui <bangzewu@126.com>
2018-10-17 10:42:37 +0800

    add dynamic bean case

--- spring-boot/006-dynamicbean/src/main/java/com/git/hui/boot/dynamicbean/Application.java
+++ spring-boot/006-dynamicbean/src/main/java/com/git/hui/boot/dynamicbean/Application.java
@@ -1,8 +1,9 @@
 package com.git.hui.boot.dynamicbean;
 
+import com.git.hui.boot.dynamicbean.auto.AutoFacDIBean;
+import com.git.hui.boot.dynamicbean.bean.auto.AnoAutoOriginBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  *
@@ -11,19 +12,13 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    public Application(ApplicationContext applicationContext) {
-//        registerManualBean((ConfigurableApplicationContext) applicationContext);
-    }
+    public Application(AutoFacDIBean autoFacDIBean, AnoAutoOriginBean anoAutoOriginBean) {
+        String str = autoFacDIBean.print();
+        System.out.println(str);
 
-//    private void registerManualBean(ConfigurableApplicationContext applicationContext) {
-//        // 主动注册一个没什么依赖的Bean
-//        ManualBean manualBean = ManualRegistBeanUtil.registerBean(applicationContext, "manualBean", ManualBean.class);
-//        manualBean.print("test print manualBean");
-//
-//        ManualDIBean manualDIBean = ManualRegistBeanUtil.registerBean(applicationContext, "manualDIBean",
-//                ManualDIBean.class, "依赖OriginBean的自定义Bean");
-//        manualDIBean.print("test print manualDIBean");
-//    }
+        String m = anoAutoOriginBean.print();
+        System.out.println(m);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ spring-boot/006-dynamicbean/src/main/java/com/git/hui/boot/dynamicbean/example/api/IUserService.java
@@ -0,0 +1,10 @@
+package com.git.hui.boot.dynamicbean.example.api;
+
+/**
+ * Created by @author yihui in 17:05 18/10/16.
+ */
+public interface IUserService {
+
+    Integer getUserId(String uname);
+
+}
