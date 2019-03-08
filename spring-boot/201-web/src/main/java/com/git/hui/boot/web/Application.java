67e5bff2b4195d3d6c9f39215bf9c8edcc19bbe5
yihui <bangzewu@126.com>
2019-03-08 10:14:49 +0800

    restTemplate https访问

--- spring-boot/201-web/src/main/java/com/git/hui/boot/web/Application.java
+++ spring-boot/201-web/src/main/java/com/git/hui/boot/web/Application.java
@@ -2,12 +2,30 @@ package com.git.hui.boot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by @author yihui in 22:01 19/2/12.
  */
 @SpringBootApplication
 public class Application {
+
+    public Application(RestTemplate restTemplate) {
+        String ans = restTemplate.getForObject("https://story.hhui.top/", String.class);
+        System.out.println(ans);
+
+
+        try {
+            ans = restTemplate.getForObject("https://47.98.136.120/", String.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
---
+++ spring-boot/201-web/src/main/java/com/git/hui/boot/web/config/RestTemplateConfig.java
@@ -0,0 +1,40 @@
+package com.git.hui.boot.web.config;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+/**
+ * Created by @author yihui in 09:07 19/3/8.
+ */
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext =
+                org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
+    }
+}
