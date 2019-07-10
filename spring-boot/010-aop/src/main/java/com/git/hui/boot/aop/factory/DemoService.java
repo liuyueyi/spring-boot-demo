cd8a952bcd5220d7c980748a257a03fff426369d
yihui <bangzewu@126.com>
2019-07-10 10:25:48 +0800

    remove useless code

--- spring-boot/010-aop/src/main/java/com/git/hui/boot/aop/factory/DemoService.java
+++ spring-boot/010-aop/src/main/java/com/git/hui/boot/aop/factory/DemoService.java
@@ -1,11 +1,8 @@
 package com.git.hui.boot.aop.factory;
 
-import org.springframework.stereotype.Service;
-
 /**
  * Created by @author yihui in 10:16 19/7/10.
  */
-//@Service("demoService")
 public class DemoService implements DemoInter {
     @Override
     public void showName(String name) {
