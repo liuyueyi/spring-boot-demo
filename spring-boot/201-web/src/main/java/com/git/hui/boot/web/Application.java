c1ffd2af068507a666108a8d3135a58512590c50
yihui <bangzewu@126.com>
2020-06-10 08:37:53 +0800

    remove illegal code

--- spring-boot/201-web/src/main/java/com/git/hui/boot/web/Application.java
+++ spring-boot/201-web/src/main/java/com/git/hui/boot/web/Application.java
@@ -2,11 +2,6 @@ package com.git.hui.boot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -16,15 +11,6 @@ import org.springframework.web.client.RestTemplate;
 public class Application {
 
     public Application(RestTemplate restTemplate) {
-        String ans = restTemplate.getForObject("https://story.hhui.top/", String.class);
-        System.out.println(ans);
-
-
-        try {
-            ans = restTemplate.getForObject("https://47.98.136.120/", String.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
