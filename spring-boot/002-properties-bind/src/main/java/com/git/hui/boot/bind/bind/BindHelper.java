76fe301403dfc433cc935dda12a342b7614e357e
yihui <bangzewu@126.com>
2023-05-12 22:52:52 +0800

    手动实现配置加载到指定bean类

---
+++ spring-boot/002-properties-bind/src/main/java/com/git/hui/boot/bind/bind/BindHelper.java
@@ -0,0 +1,31 @@
+package com.git.hui.boot.bind.bind;
+
+import com.git.hui.boot.bind.config.Pwd;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 手动实现配置加载配置类的方式
+ *
+ * @author YiHui
+ * @date 2023/5/12
+ */
+@Component
+public class BindHelper implements EnvironmentAware {
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @PostConstruct
+    public void bindInfo() {
+        Pwd config = Binder.get(environment).bindOrCreate("bind.auto", Pwd.class);
+        System.out.println(config);
+    }
+}
